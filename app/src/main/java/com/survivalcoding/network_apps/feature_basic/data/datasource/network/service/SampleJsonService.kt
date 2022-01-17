e24f807292a44b9b96427cc4b34596a6d75962f4
evan.969 <evan.969@kakaocorp.com>
2022-01-17 14:44:05 +0900

    viewbinding & retrofit dependency

---
+++ app/src/main/java/com/survivalcoding/network_apps/feature_basic/data/datasource/network/service/SampleJsonService.kt
@@ -0,0 +1,11 @@
+package com.survivalcoding.network_apps.feature_basic.data.datasource.network.service
+
+import com.survivalcoding.network_apps.feature_basic.domain.model.Todo
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+interface SampleJsonService {
+
+    @GET("todos/{id}")
+    suspend fun getTodo(@Path("id") id: String): Todo
+}

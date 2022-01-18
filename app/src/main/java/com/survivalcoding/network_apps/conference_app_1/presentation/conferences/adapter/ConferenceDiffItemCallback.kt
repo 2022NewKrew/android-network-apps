a6e9f2856450b75f1a89bb3cc964447cb6c7e6da
fac.toriall <fac.toriall@kakaocorp.com>
2022-01-18 22:33:18 +0900

    diffUtil 수정

--- app/src/main/java/com/survivalcoding/network_apps/conference_app_1/presentation/conferences/adapter/ConferenceDiffItemCallback.kt
+++ app/src/main/java/com/survivalcoding/network_apps/conference_app_1/presentation/conferences/adapter/ConferenceDiffItemCallback.kt
@@ -5,7 +5,7 @@ import com.survivalcoding.network_apps.conference_app_1.domain.model.Conference
 
 object ConferenceDiffItemCallback : DiffUtil.ItemCallback<Conference>() {
     override fun areItemsTheSame(oldItem: Conference, newItem: Conference): Boolean {
-        return oldItem.name == newItem.name
+        return oldItem.hashCode() == newItem.hashCode()
     }
 
     override fun areContentsTheSame(oldItem: Conference, newItem: Conference): Boolean {

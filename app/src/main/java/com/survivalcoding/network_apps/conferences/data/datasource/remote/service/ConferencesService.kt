28a4d98c71750df9864aa0c2e4b453ac480f110f
iryk.h <iryk.h@kakaocorp.com>
2022-01-18 13:17:13 +0900

    컨퍼런스 information ui, link

--- app/src/main/java/com/survivalcoding/network_apps/conferences/data/datasource/remote/service/ConferencesService.kt
+++ app/src/main/java/com/survivalcoding/network_apps/conferences/data/datasource/remote/service/ConferencesService.kt
@@ -4,10 +4,10 @@ import com.survivalcoding.network_apps.conferences.domain.model.Conference
 import retrofit2.http.GET
 
 interface ConferencesService {
-    @GET("/conferences.json")
+    @GET("/junsuk5/mock_json/main/conferences.json")
     suspend fun getConferences(): ArrayList<Conference>
 
     companion object {
-        const val CONFERENCES_BASE_URL = "https://raw.githubusercontent.com/junsuk5/mock_json/main"
+        const val CONFERENCES_BASE_URL = "https://raw.githubusercontent.com"
     }
 }

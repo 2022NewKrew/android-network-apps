ad913193637cd988a3d666dbbcb4e58a951e4059
jionchu <fn0809@gmail.com>
2022-01-20 21:28:34 +0900

    loading, error 이벤트 처리

--- app/src/main/java/com/survivalcoding/network_apps/feature_paging/presentation/PostsActivity.kt
+++ app/src/main/java/com/survivalcoding/network_apps/feature_paging/presentation/PostsActivity.kt
@@ -2,10 +2,12 @@ package com.survivalcoding.network_apps.feature_paging.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.core.view.isVisible
 import androidx.lifecycle.lifecycleScope
+import androidx.paging.LoadState
+import com.google.android.material.snackbar.Snackbar
+import com.survivalcoding.network_apps.R
 import com.survivalcoding.network_apps.databinding.ActivityPostsBinding
 import com.survivalcoding.network_apps.feature_paging.data.datasource.remote.RemotePostDataSource
 import com.survivalcoding.network_apps.feature_paging.data.repository.PostRepositoryImpl
@@ -27,13 +29,24 @@ class PostsActivity : AppCompatActivity() {
         // recycler view 설정
         binding.postsRecyclerView.adapter = adapter
 
-        viewModel.state.observe(this) { state ->
-            // progress bar 설정
-            binding.postsProgressBar.isVisible = state.isLoading
+        lifecycleScope.launch {
+            adapter.loadStateFlow.collectLatest {
+                // loading progress bar 설정
+                binding.postsProgressBar.isVisible = it.refresh is LoadState.Loading
+
+                // error 발생 시 알림
+                if (it.refresh::class == LoadState.Error::class) {
+                    Snackbar.make(
+                        binding.root,
+                        getString(R.string.error_network),
+                        Snackbar.LENGTH_LONG
+                    ).show()
+                }
+            }
         }
 
-        // post list 업데이트 관찰
         lifecycleScope.launch {
+            // post list 업데이트 관찰
             viewModel.posts.collectLatest { pagingData ->
                 adapter.submitData(pagingData)
             }

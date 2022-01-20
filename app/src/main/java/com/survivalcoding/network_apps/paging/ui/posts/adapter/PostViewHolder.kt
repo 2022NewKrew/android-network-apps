9f860496297f7e0eb1f4ea49068aead63f290178
shy.yi <shy.yi@kakaocorp.com>
2022-01-20 20:41:19 +0900

    Implement Basic Body Expand Feature

--- app/src/main/java/com/survivalcoding/network_apps/paging/ui/posts/adapter/PostViewHolder.kt
+++ app/src/main/java/com/survivalcoding/network_apps/paging/ui/posts/adapter/PostViewHolder.kt
@@ -7,12 +7,23 @@ import com.survivalcoding.network_apps.paging.domain.model.Post
 class PostViewHolder(private val binding: ListItemPostBinding) :
     RecyclerView.ViewHolder(binding.root) {
 
+    companion object {
+        const val COLLAPSED_LINES = 1
+        const val EXPANDED_LINES = 10
+    }
+
     fun bind(post: Post?) {
         post?.run {
             binding.indexTv.text = id.toString()
             binding.titleTv.text = title
             binding.bodyTv.text = body
+            binding.bodyTv.maxLines = COLLAPSED_LINES
             binding.userNameTv.text = post.userName ?: ""
+        }
+
+        binding.root.setOnClickListener {
+            binding.bodyTv.maxLines =
+                if (binding.bodyTv.maxLines > COLLAPSED_LINES) COLLAPSED_LINES else EXPANDED_LINES
         }
     }
 }

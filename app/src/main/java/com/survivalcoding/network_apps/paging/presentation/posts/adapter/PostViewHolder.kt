96bbb31f84eac9730e18411a98f2e33f6f95e8c5
fac.toriall <fac.toriall@kakaocorp.com>
2022-01-23 14:25:37 +0900

    아이템 UI 변경

--- app/src/main/java/com/survivalcoding/network_apps/paging/presentation/posts/adapter/PostViewHolder.kt
+++ app/src/main/java/com/survivalcoding/network_apps/paging/presentation/posts/adapter/PostViewHolder.kt
@@ -2,6 +2,7 @@ package com.survivalcoding.network_apps.paging.presentation.posts.adapter
 
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
+import com.survivalcoding.network_apps.R
 import com.survivalcoding.network_apps.databinding.ItemPostBinding
 import com.survivalcoding.network_apps.paging.domain.model.PostWithName
 
@@ -12,7 +13,7 @@ class PostViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         binding.idText.text = post.id.toString()
         binding.bodyText.text = post.body
         binding.titleText.text = post.title
-        binding.nameText.text = post.user
+        binding.nameText.text = itemView.resources.getString(R.string.user, post.user)
         itemView.setOnClickListener {
             binding.bodyText.maxLines = if (binding.bodyText.maxLines == 1) BODY_MAX else 1
             binding.titleText.maxLines = if (binding.titleText.maxLines == 1) TITLE_MAX else 1
